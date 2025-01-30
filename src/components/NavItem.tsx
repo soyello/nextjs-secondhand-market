302712e91d36d9634f7eec6921cc3bc018f0b67e
soyello <wodus9193@naver.com>
2025-01-30 11:52:57 +0900

    Navbar, NavItem 생성

---
+++ src/components/NavItem.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+import React from 'react';
+
+type navItemsProps =
+  | { href: string; label: string; type: 'link' }
+  | { label: string; type: 'button'; onClick: () => void };
+
+const NavItem = ({ mobile }: { mobile?: boolean }) => {
+  const navItems: navItemsProps[] = [
+    { href: '/admin', label: 'Admin', type: 'link' },
+    { href: '/user', label: 'User', type: 'link' },
+    { label: 'SignOut', type: 'button', onClick: () => console.log('Bye') },
+    { label: 'SignIn', type: 'button', onClick: () => console.log('Hello') },
+  ];
+  return (
+    <ul className={`flex items-center justify-center gap-4 tex-md w-full ${mobile && 'flex-col h-full'}`}>
+      {navItems.map((item, index) => (
+        <div key={index} className='py-2 border-b-2 border-selected cursor-pointer text-center'>
+          {item.type === 'link' ? (
+            <Link href={item.href}>{item.label}</Link>
+          ) : (
+            <button onClick={item.onClick}>{item.label}</button>
+          )}
+        </div>
+      ))}
+    </ul>
+  );
+};
+
+export default NavItem;
